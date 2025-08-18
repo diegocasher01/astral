f0369a6f8bcf1af496f74f14eab2a5d951ba43b0
luismiguel-jpg <luismigueluke@gmail.com>
2025-08-18 20:56:24 +0000

    siii

---
+++ servidor_minecraft/kubejs/client_scripts/ponders/distillation.js
@@ -0,0 +1,9 @@
+(function distilleryPonder() {
+  onEvent("ponder.registry", (event) => {
+    event
+      .create("createastral:distillery_dummy")
+      .scene("distillery", "The Distillery", "kubejs:distillation", (scene, util) => {
+        scene.showStructure();
+      });
+  });
+})();
---
+++ servidor_minecraft/kubejs/client_scripts/step_up_fix.js
@@ -0,0 +1,19 @@
+(function stepUpFix() {
+  function getPlayerStepHeight(player) {
+    const DEFAULT_STEP_HEIGHT = 0.6;
+    // Type assertion for now, maybe there's a more type safe way
+    const leggingsNBT = player.legsArmorItem.nbt;
+    const leggingsTag = leggingsNBT?.tag;
+    if (leggingsTag == undefined) return DEFAULT_STEP_HEIGHT;
+    const leggingsModifiers = leggingsTag.tic_modifiers;
+    if (leggingsModifiers == undefined) return DEFAULT_STEP_HEIGHT;
+    const stepUp = leggingsModifiers.filter((modifier) => modifier.name == "tconstruct:step_up")[0];
+    if (stepUp == undefined) return DEFAULT_STEP_HEIGHT;
+    return DEFAULT_STEP_HEIGHT + 0.5 * stepUp.level;
+  }
+  onEvent("client.tick", (event) => {
+    const player = event.getPlayer();
+    if (player == undefined) return;
+    player.stepHeight = getPlayerStepHeight(player);
+  });
+})();
